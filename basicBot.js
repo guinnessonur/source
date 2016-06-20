eac59a08fb6582c779e4852d32936e42153636c7
Onur GÜNEŞ <onur.gunes@tedu.edu.tr>
2016-06-21 02:04:44 +0300

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -240,7 +240,7 @@
         status: false,
         name: "basicBot",
         loggedInID: null,
-        scriptLink: "https://rawgit.com/basicBot/source/master/basicBot.js",
+        scriptLink: "https://rawgit.com/guinnessonur/source/master/basicBot.js",
         cmdLink: "http://git.io/245Ppg",
         chatLink: "https://rawgit.com/guinnessonur/source/master/lang/en.json",
         chat: null,
@@ -251,7 +251,7 @@
             botName: "basicBot",
             language: "english",
             chatLink: "https://rawgit.com/guinnessonur/source/master/lang/en.json",
-            scriptLink: "https://rawgit.com/basicBot/source/master/basicBot.js",
+            scriptLink: "https://rawgit.com/guinnessonur/source/master/basicBot.js",
             roomLock: false, // Requires an extension to re-load the script
             startupCap: 1, // 1-200
             startupVolume: 0, // 0-100
